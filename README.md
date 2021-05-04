27e96afa7b5b17dabece22ea3359ef634bde6c1c
danhdds <danhdds@gmail.com>
2021-05-04 01:49:13 -0300

    Change port to initial deploy on Heroku.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sample_node_project04
 This is a simple api to demonstrate html parse using node.
+
+
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 5000; // 5000 on local env
+const PORT = process.env.PORT || 8080; // 5000 on local env
 
 const { appRouter } = require('./routes');
 
